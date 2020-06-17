deb9d17dba0e6ce567088b833c6dbc464fb9b01a
sultanol97 <36274919+sultanol97@users.noreply.github.com>
2020-06-17 12:09:04 -0400

    working on the ui have comonens there next up is styling

--- App.js
+++ App.js
@@ -8,107 +8,120 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
+
   ScrollView,
   View,
+  Image,	 
   Text,
-  StatusBar,
+  TextInput,
+  FlatList,
+  StyleSheet
+
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+function Menu_Header() {
+  return(
+      <View 
+        style={{
+          flexDirection: "row",
+          height: 25,
+          padding: 1
+        }}
+      >
+        <Text  style={{ flex: 0.4}}>Categories </Text>
+        <Text  style={{ flex: 0.6}}>Menu Items </Text>
+      </View>  
   );
-};
+ }
+ const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
 
+function Category_Items(){
+  return(
+    <View 
+      style={{
+       flexDirection: "row",
+       height: 25,
+        padding: 1
+      }}
+    >
+    <FlatList  style={{ flex: 0.4}} git 
+      renderItem={({ item }) => <Item title={item.title} />}
+      keyExtractor={item => item.id}
+    />
+    <FlatList  style={{ flex: 0.6}} data={DATA} horizontal={true} 
+      renderItem={({ item }) => <Item title={item.title} />}
+      keyExtractor={item => item.id}
+    />
+  </View>    
+  )
+  
+}
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  item: {
+
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  title: {
+    fontSize: 32,
   },
 });
 
-export default App;
+function Popularity() {
+  return(
+    <View>
+      <Text style={{flex: 0.5}}>Most Popular for the week</Text>
+      <Text style={{flex: 0.5}}>Least Popular for the week</Text>
+    </View>
+  );
+}
+function Homeless_Donation() {
+  return(
+    <View>
+      <Text > Click to donate remaining dishes to the less fortunate</Text>
+    </View>
+  );
+}
+
+
+
+
+export default function App(){
+  return(
+	  <ScrollView>
+	  	<View>
+	  		<Text> Enter the image you see below </Text>
+	  		<Image  source={{uri: "https://reactnative.dev/docs/assets/p_cat2.png"}} style={{width: 200, height: 200}}/>
+	    	</View>
+	  	<TextInput
+	  		style={{height: 40, borderColor: 'gray', borderWidth: 1 }}
+	        	defaultValue="Enter what you see here"
+	        />
+       <Menu_Header />
+       <Category_Items />
+       <Popularity />
+       <Homeless_Donation />
+
+	  </ScrollView>
+ 
+ );
+}
