c32d559c722fd60c9007f06630353d6261e087f2
sultanol97 <36274919+sultanol97@users.noreply.github.com>
2020-06-18 11:42:29 -0400

    colors added work on spacing and typography next

---
+++ src/style/color.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const COLORS = {
+    pastel_blue: '#F4EEFF',
+    cold_blue: '#DCD6F7',
+    Blue: '#A6B1E1',
+    Purple: '#424874'
+}
+export default COLORS;
+
+
+
+
+
+
+
+
---
+++ src/style/spacing.js
@@ -0,0 +1,19 @@
+//spacing between text
+
+const Largest_padding={
+
+}
+const Smallest={
+
+}
+const Small={
+
+}
+const Large={
+
+}
+const Base={
+        paddingTop: 10,
+        paddingBottom: 5,
+}
+
